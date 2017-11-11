9798917f3d0ce15d7f95f9f2fdab8f070ae02faa
rosanna20736 <r.nichols20736@gmail.com>
2017-11-11 14:58:29 +0000

    Buttons directly connected to Pi
    attempt 1

--- src/main.py
+++ src/main.py
@@ -1,8 +1,32 @@
 import requests
-
+import RPi.GPIO as GPIO
+import time
 
 myurl = 'https://maker.ifttt.com/trigger/button_pressed/with/key/pdleupkii6JTCj0vkLYJPGu8SFVJqDRyF3cvd6rNkg5'
 
-requests.get(myurl, params = {'value1' : 'Blue'})
+blue_channel = 15
+red_channel = 14
+purple_channel = 2
+black_channel = 18
+channel_list = [blue_channel, red_channel, purple_channel, black_channel]
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(blue_channel, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
+for channel in range(0, len(channel_list)):
+    GPIO.add_event_detected(channel_list[channel], GPIO.RISING)
+
+while True:
+    time.sleep(0.2)
+
+    if GPIO.event_detected(blue_channel):
+        requests.get(myurl, params = {'value1' : 'Big blue'})
+
+    elif GPIO.event_detected(red_channel):
+        requests.get(myurl, params = {'value1' : 'Big red'})
+
+    elif GPIO.event_detected(purple_channel):
+        requests.get(myurl, params={'value1': 'Small purple'})
+
+    elif GPIO.event_detected(black_channel):
+        requests.get(myurl, params = {'value1' : 'Small black'})
